66f2fac13b7548878a3b76c840f7535cf7bbedf0
Felipe Gauer <felipe.gattelli@edu.pucrs.br>
2022-05-20 11:05:06 -0300

    Cartas maiores que 10 = 10.

--- Blackjack.java
+++ Blackjack.java
@@ -12,31 +12,59 @@ public class Blackjack
     public static int gerarCarta(Random r) {
         int carta = r.nextInt(13)+ 1;
         String card="";
-        if (carta>10||carta==1)
+        if (carta>10||carta==1){
             card=mostrarCarta(carta);
+            switch(card){
+                case "J":
+                    carta=10;
+                    break;
+                case "Q":
+                    carta=10;
+                    break;
+                case "K":
+                    carta=10;
+                    break;
+            }
+        }
         mostrar(card,carta);
         return carta;
     }
-    public static String mostrarCarta(int carta){
-        Scanner in = new Scanner(System.in);
-        int A;
+    public static int gerarMesa(Random r){
+        int carta = r.nextInt(13)+1;
         String card="";
-        if(carta==11)
-            card="J";
-        else if(carta ==12)
-            card="Q";
-        else if(carta == 13)
-            card="K";
-        else if(carta==1){
-            System.out.println("Você pegou um ÁS, quer que seja 1 ou 11?");
-            A=in.nextInt();
-            if(A==1)
-                card="A(1)";
-            else if(A==11)
-                card="A(11)";    
+        if (carta>10||carta==1){
+            card=mostrarCarta(carta);
+            switch(card){
+                case "J":
+                    carta=10;
+                    break;
+                case "Q":
+                    carta=10;
+                    break;
+                case "K":
+                    carta=10;
+                    break;
+            }
         }
-        return card;
-        
+        return carta;
+    }
+    public static String mostrarCarta(int carta){
+        String card="";
+       switch (carta){
+           case 1:
+               card="A";
+               break;
+            case 11:
+                card="J";
+                break;
+            case 12:
+                card="Q";
+                break;
+            case 13:
+                card="K";
+                break;
+       }
+       return card;
     }
     public static void mostrar(String card, int carta){
         if(card.equals(""))
@@ -49,6 +77,7 @@ public class Blackjack
         Scanner in = new Scanner(System.in);
         String perg="";
         do{
+            System.out.println("Quer outra carta?(s/n)");
             perg = in.nextLine(); 
             if(perg.equals("s") || perg.equals("S")){
                 j3= j3 + gerarCarta(r);
@@ -58,17 +87,28 @@ public class Blackjack
         in.close();
         return j3;
     }
-    
+    public static int outraCartaMesa(int c1,int c2,int j1,int j2,int j3,Random r){
+        int c3=0;
+        while(j1+j2+j3 > c1+c2 && j1+j2+j3<=21){
+            if(c3>21 || c1+c2+c3>j1+j2+j3)
+                break;
+            else
+                c3=c3 + gerarCarta(r);
+        }
+        return c3;
+    }
     public static void main(String[] args) {
         Random r;
-        int c1, c2;
+        int c1, c2,c3;
         int j1, j2,j3;
         j3=0;
+        c3=0;
+        String card="";
         
         r = new Random();
         System.out.printf("COMPUTADOR%n");        
         c1 = gerarCarta(r);
-        c2 = gerarCarta(r);
+        
         System.out.printf("total(sem a segunda carta):%d%n",c1);
         
         System.out.printf("HUMANO%n");
@@ -78,9 +118,21 @@ public class Blackjack
 
 
         System.out.println("Total:"+(j1+j2));
-        System.out.println("Quer outra carta?");
         
         j3=outracarta(j1,j2,j3,r);
+        System.out.println("-----------------------");
+        System.out.println("Computador:");
+        
+        c2 = gerarMesa(r);
+        mostrar(card,c2);
+        
+        if(j1+j2+j3 > c1+c2 && j1+j2+j3<=21){
+            c3= outraCartaMesa(c1,c2,j1,j2,j3,r);
+        }
+        
+        System.out.printf("total computador: %d%n", c1+c2+c3);
+        System.out.println("-----------------------");
+        
         if(j1+j2+j3 > c1+c2 && j1+j2+j3<=21 ){
             System.out.println("GANHOU!");
         }else{
